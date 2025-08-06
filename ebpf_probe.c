e29c26d6ac4ca8ac7ef6277225fdeb3de76626ec
lioriz <lioriz888@gmail.com>
2025-08-06 20:30:13 +0300

    Cleare logs and o0ld traces

--- ebpf_probe.c
+++ ebpf_probe.c
@@ -5,8 +5,10 @@ BPF_HASH(skip_pid, u32, u32);
 
 int sys_read_enter(struct pt_regs *ctx, int fd, char __user *buf, size_t count)
 {
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
-    if (skip_pid.lookup(&pid)) {
+    u32 pid = bpf_get_current_pid_tgid();
+    u32 *val = skip_pid.lookup(&pid);
+    if (val) {
+        // bpf_trace_printk("sys_read skip pid %d\n", *val);
         return 0;
     }
     bpf_trace_printk("hello sys_read was called\n");
@@ -15,8 +17,10 @@ int sys_read_enter(struct pt_regs *ctx, int fd, char __user *buf, size_t count)
 
 int sys_write_enter(struct pt_regs *ctx, int fd, const char __user *buf, size_t count)
 {
-    u32 pid = bpf_get_current_pid_tgid() >> 32;
-    if (skip_pid.lookup(&pid)) {
+    u32 pid = bpf_get_current_pid_tgid();
+    u32 *val = skip_pid.lookup(&pid);
+    if (val) {
+        // bpf_trace_printk("sys_write skip pid %d\n", *val);
         return 0;
     }
     bpf_trace_printk("hello sys_write was called\n");
--- run_probe.py
+++ run_probe.py
@@ -4,25 +4,27 @@ import sys
 import time
 import os
 
+
+# Clear old traces
+os.system("echo > /sys/kernel/debug/tracing/trace")
+
 # Load the eBPF program
 b = BPF(src_file="ebpf_probe.c")
-# b["events"].open_perf_buffer(print_event, page_cnt=256)
 
-print("Loding eBPF program. Press Ctrl+C to exit.")
+# Map to skip self process
+skip_map = b["skip_pid"]
+pid = os.getpid()
+c_pid = ctypes.c_uint(pid)
+skip_map[c_pid] = c_pid
+
+print("Loding eBPF program")
 print("Monitoring sys_read and sys_write calls...")
+print(f"Host PID: {pid}")
+print(f"Skipping self PID: {pid}")
 
 # Attach kprobes
 b.attach_kprobe(event="__x64_sys_read", fn_name="sys_read_enter")
 b.attach_kprobe(event="__x64_sys_write", fn_name="sys_write_enter")
-
-# Map to skip self process
-skip_map = b["skip_pid"]
-
-pid = os.getpid()
-print(f"Host PID: {pid}")
-print(f"Skipping self PID: {pid}")
-c_pid = ctypes.c_uint(pid)
-skip_map[c_pid] = c_pid  # BPF maps require ctypes
 
 try:
     b.trace_print()
